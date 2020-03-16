73d47ed2bfb6234c6fbaf6fa25b1ed1c576ec93f
Daniela Milena Guerrero Pérez <onix.dg@MacBook-Pro-de-Daniela.local>
2020-03-16 00:56:44 -0300

    Added Routes in App and Added bootstrap to the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import Navigation from './components/Navigation';
+import CreateAnimal from './components/CreateAnimal';
+import GetAnimals from './components/GetAnimals';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navigation/>
+      <Route path="/" exact component={GetAnimals} />
+      <Route path="/edit/:id" component={CreateAnimal} />
+      <Route path="/create" component={CreateAnimal} />
+    </Router>
   );
 }
 
---
+++ src/components/CreateAnimal.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class CreateAnimal extends Component {
+    render() {
+        return(
+            <div>
+                CREATE ANIMAL
+            </div>
+        )
+
+    }
+}
