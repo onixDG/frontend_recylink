e4b8f4ae8c7dd87ad9daaffb887950a7a0dcdf9b
Daniela Milena Guerrero Pérez <onix.dg@MacBook-Pro-de-Daniela.local>
2020-03-17 15:23:10 -0300

    Implemented function GetAnimals

--- src/components/CreateAnimal.js
+++ src/components/CreateAnimal.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class CreateAnimal extends Component {
+
     render() {
         return(
             <div>
--- src/components/GetAnimals.js
+++ src/components/GetAnimals.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react'
+import axios from "axios";
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+
+
+
 
 export default class GetAnimals extends Component {
+
+    state = {
+        animals: []
+    }
+    async componentDidMount() {
+        const res = await axios.get('http://localhost:4000/api/animals');
+        this.setState({animals: res.data})
+        console.log(this.state.animals)
+    }
     render() {
         return(
-            <div>
-                ANIMAL LIST
-            </div>
+            <>
+                <Table striped bordered hover>
+                  <thead>
+                    <tr>
+                      <th>#</th>
+                      <th>Name</th>
+                      <th>Creation date</th>
+                      <th>Actions</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {
+                        this.state.animals.map(animal => (
+                            <tr>
+                                  <td>{animal._id}</td>
+                                  <td>{animal.name}</td>
+                                  <td>{animal.date}</td>
+                                  <td>
+                                      <Button variant="outline-warning">Editar</Button>{' '}
+                                      <Button variant="outline-danger">Eliminar</Button>{' '}
+                                  </td>
+                            </tr>
+                                ))
+                            }
+                  </tbody>
+                </Table>
+
+
+             </>
+
+
+
+
+
+
+
         )
 
     }
